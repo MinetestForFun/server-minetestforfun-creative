904bb60164d1a488286a35f881cacfb974271b5e
LeMagnesium <mg.minetest@gmail.com>
2015-09-17 08:48:33 +0200

    [cavestuff] Deactivate and comment cavestuff  - Solves #14

--- mods/plantlife_modpack/cavestuff/init.lua
+++ mods/plantlife_modpack/cavestuff/init.lua
@@ -1,12 +1,20 @@
 -----------------------------------------------------------------------------------------------
-local title		= "Cave Stuff"
-local version 	= "0.0.3"
-local mname		= "cavestuff"
+-- local title		= "Cave Stuff"
+-- local version 	= "0.0.3"
+-- local mname		= "cavestuff"
 -----------------------------------------------------------------------------------------------
 
-dofile(minetest.get_modpath("cavestuff").."/nodes.lua")
-dofile(minetest.get_modpath("cavestuff").."/mapgen.lua")
+-- dofile(minetest.get_modpath("cavestuff").."/nodes.lua")
+-- dofile(minetest.get_modpath("cavestuff").."/mapgen.lua")
 
 -----------------------------------------------------------------------------------------------
 
-minetest.log("action", "[Mod] "..title.." ["..version.."] ["..mname.."] Loaded...")
+-- minetest.log("action", "[Mod] "..title.." ["..version.."] ["..mname.."] Loaded...")
+
+minetest.register_alias("air", "cavestuff:pebble_1")
+minetest.register_alias("air", "cavestuff:pebble_2")
+minetest.register_alias("air", "cavestuff:desert_pebble_1")
+minetest.register_alias("air", "cavestuff:desert_pebble_2")
+minetest.register_alias("air", "cavestuff:stalactite_1")
+minetest.register_alias("air", "cavestuff:stalactite_2")
+minetest.register_alias("air", "cavestuff:stalactite_3")
