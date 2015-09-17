98ba5b78b1e0b5018024dbe0684d7dc1ab37a011
Ombridride <Ombridride@users.noreply.github.com>
2015-09-17 15:52:25 +0200

    More different introducing nodes in...
     the initial stuff

--- mods/_misc/give_initial_stuff.lua
+++ mods/_misc/give_initial_stuff.lua
@@ -2,10 +2,12 @@ minetest.register_on_newplayer(function(player)
 	print("Un nouveau joueur vient de nous rejoindre !")
 	if minetest.setting_getbool("give_initial_stuff") then
 		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
-		player:get_inventory():add_item('main', 'default:cobble 20')
-		player:get_inventory():add_item('main', 'default:tree 20')
-		player:get_inventory():add_item('main', 'default:torch 10')
-		player:get_inventory():add_item('main', 'default:sapling 5')
+		player:get_inventory():add_item('main', 'default:cobble 99')
+		player:get_inventory():add_item('main', 'colored_steel:block_blue 99')
+		player:get_inventory():add_item('main', 'default:torch 99')
+		player:get_inventory():add_item('main', 'default:cherry_plank 99')
+		player:get_inventory():add_item('main', 'bakedclay:magenta 99')
+		player:get_inventory():add_item('main', 'moreblocks:all_faces_tree 99')
 	end
 end)
 
