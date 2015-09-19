0c4662ae049fb22a8b7ba4df252ef45aef6b05bd
LeMagnesium <mg.minetest@gmail.com>
2015-09-19 21:38:51 +0200

    [default/nodes] Fix commas

--- minetestforfun_game/mods/default/nodes.lua
+++ minetestforfun_game/mods/default/nodes.lua
@@ -1423,7 +1423,7 @@ minetest.register_node("default:water_flowing", {
 	liquid_alternative_flowing = "default:water_flowing",
 	liquid_alternative_source = "default:water_source",
 	liquid_viscosity = 1,
-	liquid_range = 3
+	liquid_range = 3,
 	post_effect_color = {a = 120, r = 20, g = 60, b = 80},
 	groups = {water = 3, liquid = 3, puts_out_fire = 1,
 		not_in_creative_inventory=1},
@@ -1469,7 +1469,6 @@ minetest.register_node("default:river_water_source", {
 	liquid_alternative_flowing = "default:river_water_flowing",
 	liquid_alternative_source = "default:river_water_source",
 	liquid_viscosity = 1,
-	liquid_range = 3
 	liquid_renewable = false,
 	liquid_range = 2,
 	post_effect_color = {a = 64, r = 100, g = 100, b = 200},
@@ -1517,7 +1516,6 @@ minetest.register_node("default:river_water_flowing", {
 	liquid_alternative_flowing = "default:river_water_flowing",
 	liquid_alternative_source = "default:river_water_source",
 	liquid_viscosity = 1,
-	liquid_range = 3
 	liquid_renewable = false,
 	liquid_range = 2,
 	post_effect_color = {a = 64, r = 100, g = 100, b = 200},
@@ -1567,7 +1565,7 @@ minetest.register_node("default:lava_source", {
 	liquid_alternative_flowing = "default:lava_flowing",
 	liquid_alternative_source = "default:lava_source",
 	liquid_viscosity = 7,
-	liquid_range = 3
+	liquid_range = 3,
 	liquid_renewable = false,
 	damage_per_second = 4 * 2,
 	post_effect_color = {a = 220, r = 250, g = 70, b = 20},
@@ -1615,7 +1613,7 @@ minetest.register_node("default:lava_flowing", {
 	liquid_alternative_flowing = "default:lava_flowing",
 	liquid_alternative_source = "default:lava_source",
 	liquid_viscosity = 7,
-	liquid_range = 3
+	liquid_range = 3,
 	liquid_renewable = false,
 	damage_per_second = 4 * 2,
 	post_effect_color = {a = 220, r = 250, g = 70, b = 20},
