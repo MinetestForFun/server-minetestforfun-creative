60a287eab06b97508752716586a281a1af5eebfa
LeMagnesium <mg.minetest@gmail.com>
2015-11-21 12:35:59 +0100

    [mesecons] Add NOR and OR gates, and documentation

--- mods/mobs/init.lua
+++ mods/mobs/init.lua
@@ -40,7 +40,7 @@ dofile(path.."/minotaur.lua") -- Kalabasa
 -- cannot find mesecons?, craft glue instead
 if not minetest.get_modpath("mesecons_materials") then
 	minetest.register_craftitem(":mesecons_materials:glue", {
-		image = "jeija_glue.png",
+		image = "mesecons_glue.png",
 		description = "Glue",
 	})
 end
