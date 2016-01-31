23de56d7bd57ea175f15ad2002671b5f9e66196a
LeMagnesium <mg.minetest@gmail.com>
2016-01-31 15:42:45 +0100

    [plantlife_modpack] Correct mushroom aliases and fix cavestuff
    - Create aliases for cavestuff nodes instead of using an ABM
    - Apply the same fix as on Classic for mushroom aliasing

--- mods/plantlife_modpack/cavestuff/init.lua
+++ mods/plantlife_modpack/cavestuff/init.lua
@@ -11,11 +11,15 @@
 
 -- minetest.log("action", "[Mod] "..title.." ["..version.."] ["..mname.."] Loaded...")
 
-minetest.register_abm({
+--[[minetest.register_abm({
 	nodes = {"cavestuff:pebble_1", "cavestuff:pebble_2", "cavestuff:desert_pebble_1", "cavestuff:desert_pebble_2", "cavestuff:stalactite_1", "cavestuff:stalactite_2", "cavestuff:stalactite_3"},
 	interval = 1,
 	chance = 1,
 	action = function(pos)
 		minetest.set_node(pos, {name = "default:grass_2"})
 	end,
-})
+})]]
+
+for _, node in pairs({"cavestuff:pebble_1", "cavestuff:pebble_2", "cavestuff:desert_pebble_1", "cavestuff:desert_pebble_2", "cavestuff:stalactite_1", "cavestuff:stalactite_2", "cavestuff:stalactite_3"}) do
+	minetest.register_alias(node, "air")
+end
